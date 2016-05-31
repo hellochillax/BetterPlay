326a8880abd32e9a28024243b10a6292890a3303
cm1996 <chenmo1996@outlook.com>
2016-05-31 21:08:44 +0800

    orders' linking to bomb

--- app/src/main/java/wang/chillax/betterplay/fragment/PaidPage.java
+++ app/src/main/java/wang/chillax/betterplay/fragment/PaidPage.java
@@ -1,29 +1,38 @@
 package wang.chillax.betterplay.fragment;
 
 
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import com.yalantis.taurus.PullToRefreshView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
-import in.srain.cube.views.ptr.PtrFrameLayout;
-import in.srain.cube.views.ptr.PtrHandler;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.listener.FindListener;
 import wang.chillax.betterplay.R;
 import wang.chillax.betterplay.adapter.BaseAdapter;
 import wang.chillax.betterplay.adapter.ViewHolder;
 import wang.chillax.betterplay.bmob.Order;
+import wang.chillax.betterplay.bmob.User;
 
 /**
- * 已支付界面, 你还要继承一下那个下拉刷新的接口 我暂时先不写
+ * 已支付界面
  */
-public class PaidPage extends BasePage implements PtrHandler{
+public class PaidPage extends BasePage{
         @Bind(R.id.lv_paid)
         ListView mListView;
+        @Bind(R.id.pull_to_refresh)
+        PullToRefreshView mPtrView;
         List<Order> mOrders = new ArrayList<Order>();
         OrderAdapter mOrderAdapter;
+        User user;
+
 
         @Override
         public int initLayoutRes() {
@@ -32,28 +41,50 @@ public class PaidPage extends BasePage implements PtrHandler{
 
         @Override
         public void initViews() {
+
+                mPtrView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
+                        @Override
+                        public void onRefresh() {
+                                getNewData();
+                        }
+                });
         }
 
         @Override
         public void initDatas() {
-                mOrders.add(new Order("","",1,"",1,1.0));
-                mOrders.add(new Order("","",1,"",1,1.0));
-                mOrders.add(new Order("","",1,"",1,1.0));
+                user = BmobUser.getCurrentUser(super.context,User.class);
                 mOrderAdapter = new OrderAdapter(mOrders);
                 mListView.setAdapter(mOrderAdapter);
+                getNewData();
+                mOrderAdapter.notifyDataSetChanged();
 
         }
 
-        @Override
-        public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
-                return false;
+        public void getNewData() {
+                BmobQuery<Order> query = new BmobQuery<Order>();
+                query.addWhereEqualTo("username", user.getPhone());
+                query.addWhereEqualTo("status", 1);
+                query.findObjects(super.context, new FindListener<Order>() {
+                        @Override
+                        public void onSuccess(List<Order> list) {
+                                mOrders.clear();
+                                for(Order o: list) {
+                                        mOrders.add(o);
+                                }
+                                //mOrders = list;
+                                //showToast("成功");
+                                Log.e("fffff*****",""+mOrders.size());
+                                mPtrView.setRefreshing(false);
+                        }
+
+                        @Override
+                        public void onError(int i, String s) {
+                                showToast("失败");
+                                mPtrView.setRefreshing(false);
+                        }
+                });
+                mOrderAdapter.notifyDataSetChanged();
         }
-
-        @Override
-        public void onRefreshBegin(PtrFrameLayout frame) {
-
-        }
-
         class OrderAdapter extends BaseAdapter{
 
                 List<Order> mList;
@@ -68,10 +99,13 @@ public class PaidPage extends BasePage implements PtrHandler{
                 @Override
                 public View getView(int position, View convertView, ViewGroup parent) {
                         ViewHolder holder= ViewHolder.get(PaidPage.super.context,convertView,R.layout.order_list_item,position,parent);
+                        holder.setText(R.id.orderName,""+mList.get(position).getOrder_id());
+                        holder.setText(R.id.orderCount,"*"+mList.get(position).getCount());
+                        holder.setText(R.id.orderPrice,""+mList.get(position).getPrice());
                         //这个中间对东西赋值
                         return holder.getConvertView();
                         //return null;
                 }
         }
 
-    }
+}
--- app/src/main/java/wang/chillax/betterplay/fragment/UnpaidPage.java
+++ app/src/main/java/wang/chillax/betterplay/fragment/UnpaidPage.java
@@ -5,56 +5,82 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.yalantis.taurus.PullToRefreshView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
-import in.srain.cube.views.ptr.PtrFrameLayout;
-import in.srain.cube.views.ptr.PtrHandler;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.listener.FindListener;
 import wang.chillax.betterplay.R;
 import wang.chillax.betterplay.adapter.BaseAdapter;
 import wang.chillax.betterplay.adapter.ViewHolder;
 import wang.chillax.betterplay.bmob.Order;
+import wang.chillax.betterplay.bmob.User;
 
 
-public class UnpaidPage extends BasePage implements PtrHandler{
+public class UnpaidPage extends BasePage {
     @Bind(R.id.lv_unpaid)
     ListView mListView;
+    @Bind(R.id.pull_to_refresh)
+    PullToRefreshView mPtrView;
     List<Order> mOrders = new ArrayList<Order>();
     OrderAdapter mOrderAdapter;
+    User user;
+
 
     @Override
     public int initLayoutRes() {
-        Log.e("222222222","0000000000");
         return R.layout.fragment_unpaid_page;
     }
 
     @Override
     public void initViews() {
-        Log.e("222222222","1111111");
+        mPtrView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getNewData();
+            }
+        });
     }
 
     @Override
     public void initDatas() {
-        mOrders.add(new Order("","",1,"",1,1.0));
-        mOrders.add(new Order("","",1,"",1,1.0));
-        mOrders.add(new Order("","",1,"",1,1.0));
+        user = BmobUser.getCurrentUser(super.context,User.class);
         mOrderAdapter = new OrderAdapter(mOrders);
         mListView.setAdapter(mOrderAdapter);
-        Log.e("222222222","2222222222");
+        getNewData();
+        mOrderAdapter.notifyDataSetChanged();
     }
 
-    @Override
-    public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
-        return false;
+    public void getNewData() {
+        BmobQuery<Order> query = new BmobQuery<Order>();
+        query.addWhereEqualTo("username", user.getPhone());
+        query.addWhereEqualTo("status", 0);
+        query.findObjects(super.context, new FindListener<Order>() {
+            @Override
+            public void onSuccess(List<Order> list) {
+                mOrders.clear();
+                for(Order o: list) {
+                    mOrders.add(o);
+                }
+                //mOrders = list;
+                //showToast("成功");
+                Log.e("fffff*****",""+mOrders.size());
+                mPtrView.setRefreshing(false);
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                showToast("失败");
+                mPtrView.setRefreshing(false);
+            }
+        });
+        mOrderAdapter.notifyDataSetChanged();
     }
-
-    @Override
-    public void onRefreshBegin(PtrFrameLayout frame) {
-
-    }
-
-    class OrderAdapter extends BaseAdapter {
+    class OrderAdapter extends BaseAdapter{
 
         List<Order> mList;
         OrderAdapter(List<Order> list){
@@ -68,6 +94,9 @@ public class UnpaidPage extends BasePage implements PtrHandler{
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             ViewHolder holder= ViewHolder.get(UnpaidPage.super.context,convertView,R.layout.order_list_item,position,parent);
+            holder.setText(R.id.orderName,""+mList.get(position).getOrder_id());
+            holder.setText(R.id.orderCount,"*"+mList.get(position).getCount());
+            holder.setText(R.id.orderPrice,""+mList.get(position).getPrice());
             //这个中间对东西赋值
             return holder.getConvertView();
             //return null;
